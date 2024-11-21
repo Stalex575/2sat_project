5c14c1cfbd11abf11eb0478fbaf5f5659d313af6
Stalex575 <stadnick007@gmail.com>
2024-11-21 12:05:11 +0200

    minor changes to read_mods, added template functions, made read_constraints

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def read_mode(filename:str) -> dict:
+def read_mods(filename:str) -> dict:
     """
 Reads a file containing modification data and returns a dictionary
-where the key is the modification id and the value is a tuple (name, user).
-Return dictionary {key(id): (name,user)}
+where the key is the modification mod_id and the value is a tuple (name, user_visibility).
+Return dictionary {key(mod_id): (name,user_visibility)}
 :param filename: The path to the file containing the modification data.
-:param modifications_dict: A dictionary where the key is id and the value\
-is a tuple (name: str, user: int).
+:return modifications_dict: A dictionary where the key is mod_id and the value\
+is a tuple (name: str, user_visibility: int).
     """
     with open(filename, encoding='utf-8') as f:
         modifications_dict = {}
@@ -13,8 +13,36 @@ is a tuple (name: str, user: int).
         for i in f:
             i = i.strip().split(';')
             if len(i) != 3:
-                print('Неправильно написані дані,будь ласка, відкоригуйте файл!')
+                print('Неправильно написані дані, будь ласка, відкоригуйте файл!')
                 break
-            id, name, user = i
-            modifications_dict[int(id)] = (name,int(user))
+            mod_id, name, user_visibility = i
+            modifications_dict[int(mod_id)] = (name.strip(),int(user_visibility))
     return modifications_dict
+
+def read_constraints(filename: str):
+    constraints = {}
+    with open(filename, mode='r', encoding='utf-8') as f:
+        f.readline()
+        for line in f:
+            line = line.strip().split(';')
+            mod_id = int(line[0])
+            conflicts = line[1].strip().split(',')
+            requirements = line[2].strip().split(',')
+            constraints[mod_id] = ([-int(num) for num in conflicts \
+            if conflicts != ['']] + [int(num) for num in requirements if line[2] != ''])
+    return constraints
+
+def build_graph(constraints: dict[int: list[int]], user_choice: list[int]) -> dict:
+    pass
+
+def build_tarjans_sccs(graph: dict) -> list:
+    def strong_connection(v: int) -> None:
+        pass
+    pass
+
+def is_satisfiable(constraints: dict[int: list[int]], user_choice: list[int]) -> bool:
+    pass
+
+def main():
+    constraints = read_constraints('')
+    modifications = read_mods('')
